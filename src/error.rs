76c653ec00244183a487d636bb9d4f562a9cf69f
Zach Bloss <zacharybloss@gmail.com>
2021-09-14 20:42:19 -0400

    add final instruction set

--- src/error.rs
+++ src/error.rs
@@ -3,9 +3,18 @@ use solana_program::program_error::ProgramError;
 
 #[derive(Error, Debug, Copy, Clone)]
 pub enum EscrowError {
-    /// Invalid Instructions
+    /// Invalid instruction
     #[error("Invalid Instruction")]
     InvalidInstruction,
+    /// Not Rent Exempt
+    #[error("Not Rent Exempt")]
+    NotRentExempt,
+    /// Expected Amount Mismatch
+    #[error("Expected Amount Mismatch")]
+    ExpectedAmountMismatch,
+    /// Amount Overflow
+    #[error("Amount Overflow")]
+    AmountOverflow,
 }
 
 impl From<EscrowError> for ProgramError {
